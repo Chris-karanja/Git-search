9092cd3bb4d92247f167a10cc936a20b8568682b
Chris-karanja <chriswainaina0@gmail.com>
2019-05-29 13:26:28 +0300

    defined the click function of the search button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Git';
+
+  constructor(private http: Http){}
+
+  userName = '';
+  searchUser() {
+    this.http.get('https://api.github.com/users' + this.userName)
+  }
 }
