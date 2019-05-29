4328647793279995e78fcf847c73a80c30916894
Chris-karanja <chriswainaina0@gmail.com>
2019-05-29 12:55:05 +0300

    added the generated key into env.ts and updated gitignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  access_key: '021d4d836aa5b05b5463080881c55098eb1409af'
 };
 
 /*
